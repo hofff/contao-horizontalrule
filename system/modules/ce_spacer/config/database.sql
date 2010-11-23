4515e1fcef4362e08d01650a1fc1d2d01cf2815e
Tristan Lins <tristan.lins@infinitysoft.de>
2010-11-23 13:42:46 +0000

    Initial import.

---
+++ system/modules/ce_spacer/config/database.sql
@@ -0,0 +1,16 @@
+-- **********************************************************
+-- *                                                        *
+-- * IMPORTANT NOTE                                         *
+-- *                                                        *
+-- * Do not import this file manually but use the TYPOlight *
+-- * install tool to create and maintain database tables!   *
+-- *                                                        *
+-- **********************************************************
+
+-- 
+-- Table `tl_content`
+-- 
+
+CREATE TABLE `tl_content` (
+  `anchor` varchar(255) NOT NULL default '',
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ system/modules/ce_spacer/templates/ce_spacer.tpl
@@ -0,0 +1,2 @@
+<hr class="ce_spacer"/>
+<?php if (strlen($this->anchor)): ?><div class="ce_spacer"><a title="<?php echo $GLOBALS['TL_LANG']['MSC']['backToTop'] ?>" href="{{env::request}}#<?php echo $this->anchor ?>"><?php echo $GLOBALS['TL_LANG']['MSC']['backToTop'] ?></a></div><?php endif; ?>
