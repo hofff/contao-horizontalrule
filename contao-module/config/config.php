ba880b5630ae3b4b83004a8200bb0ce4303f1bc2
Oliver Hoff <oliver@hofff.com>
2015-12-19 09:04:36 +0100

    fix config

--- contao-module/config/config.php
+++ contao-module/config/config.php
@@ -3,5 +3,5 @@
 $GLOBALS['TL_CTE']['texts']['hofff_horizontalrule']
 	= 'Hofff\\Contao\\HorizontalRule\\ContentHorizontalRule';
 
-$GLOBALS['FE_FFL']['hofff_horizontalrule']
+$GLOBALS['TL_FFL']['hofff_horizontalrule']
 	= 'Hofff\\Contao\\HorizontalRule\\FormHorizontalRule';
